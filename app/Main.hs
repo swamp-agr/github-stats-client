a47bdd0a8078c7540ec0c5b00ef146f8d6e18b02
Andrey Prokopenko <persiantiger@yandex.ru>
2017-09-29 00:41:10 +0300

    [swamp-agr][add] added calls, wreq dependency..

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,13 @@
 {-# LANGUAGE DeriveGeneric #-}
 module Main where
 
-import Control.Exception (throw)
+import Control.Exception
 import Data.Yaml
-import Data.ByteString (readFile)
+import Data.ByteString (ByteString(..), readFile)
 import Prelude hiding (readFile)
 import System.Environment
 import GHC.Generics
+import Network.Wreq
 
 main :: IO ()
 main = do
@@ -38,23 +39,49 @@ execute settings "ALL" = do
   printUsers users
 execute settings range =
   case validate range of
-    (VALID,vrange) -> getUsersFromRange settings vrange >>= printUsers
+    (VALID,vrange) -> do
+      users <- getUsersFromRange settings vrange
+      printUsers users
     (INVALID,_)    -> showUsage
 
 getAllRanges :: Settings -> IO [Range]
 getAllRanges settings = undefined
 
 getUsersFromRange :: Settings -> Range -> IO [User]
-getUsersFromRange settings range = undefined
+getUsersFromRange settings range = do
+  let opts = setOpts settings
+  resp <- try $ call opts range
+  case resp of
+    Right result -> return $ decodeUsers result
+    Left err@(SomeException _)     -> return []
 
 getAllUsers :: Settings -> [Range] -> IO [User]
-getAllUsers settings ranges = undefined
+getAllUsers settings ranges = do
+  let opts = setOpts settings
+  responses <- callMany opts ranges
+  return $ concat $ fmap fromEither responses
 
 printUsers :: [User] -> IO ()
 printUsers u = undefined
 
 validate :: Command -> (Validation,Range)
 validate cmd = undefined
+
+call :: Options -> Range -> IO ByteString
+call opts range = undefined
+
+callMany :: Options -> [Range] -> IO [APIResponse]
+callMany opts ranges = undefined
+
+decodeUsers :: ByteString -> [User]
+decodeUsers resp = undefined
+
+setOpts :: Settings -> Options
+setOpts settings = undefined
+
+fromEither :: APIResponse -> [User]
+fromEither (Right usr) = usr
+fromEither (Left _) = []
 
 data Settings = Settings String deriving Generic
 instance FromJSON Settings
@@ -64,3 +91,7 @@ type Command = String
 data Validation = VALID | INVALID
 data User
 data Range
+data APICall
+
+type APIResponse = Either ErrorMessage [User]
+type ErrorMessage = ByteString
